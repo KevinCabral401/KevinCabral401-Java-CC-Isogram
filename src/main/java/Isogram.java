ef1052b07106790ee52205036b8229b7a311a1ee
Kevin Cabral <kevincabral401@gmail.com>
2024-11-14 01:55:59 +0000

    Completed Coding Challange Isogram

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Isogram {
     /**
@@ -13,6 +15,14 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        Set<Character> letters = new HashSet<>();
+        for(int i = 0; i < str.length(); i++){
+            if(letters.contains(str.charAt(i))){
+                return false;
+            }
+            letters.add(str.charAt(i));
+        }
+        
+        return true;
     }
 }
